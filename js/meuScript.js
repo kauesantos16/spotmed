334db396e4c09051e706e99b91b60d165ae0407b
kauesantos16 <120751369+kauesantos16@users.noreply.github.com>
2023-03-08 19:33:20 -0300

    Update meuScript.js

--- js/meuScript.js
+++ js/meuScript.js
@@ -1,17 +1,19 @@
-// selecione os seus menus fixos
-var menuFixoAtivo = document.querySelector('.menu-fixo-ativo');
-var menuFixoInativo = document.querySelector('.menu-fixo-inativo');
+// Adicione isso em um embed de código personalizado antes da tag </body>
 
-// salve a altura do menu fixo ativo em relação ao topo da página
-var alturaMenuFixoAtivo = menuFixoAtivo.offsetTop;
+// Seleciona os elementos com as classes de menu
+const menuPrincipal = document.querySelector('.menu-fixo-ativo');
+const menuSecundario = document.querySelector('.menu-fixo-inativo');
 
-// adicione a classe "menu-fixo-inativo-ativo" e remova a classe "menu-fixo-ativo" quando o usuário rolar a página além da altura do menu fixo ativo
-window.addEventListener('scroll', function() {
-  if (window.pageYOffset >= alturaMenuFixoAtivo) {
-    menuFixoAtivo.classList.remove('menu-fixo-ativo');
-    menuFixoInativo.classList.add('menu-fixo-inativo-ativo');
+// Detecta quando o usuário rola a página
+window.addEventListener('scroll', () => {
+  // Verifica se o usuário rolou a página além do topo do menu principal
+  if (window.scrollY > menuPrincipal.offsetTop) {
+    // Mostra o menu secundário e oculta o menu principal
+    menuSecundario.style.display = 'block';
+    menuPrincipal.style.display = 'none';
   } else {
-    menuFixoAtivo.classList.add('menu-fixo-ativo');
-    menuFixoInativo.classList.remove('menu-fixo-inativo-ativo');
+    // Mostra o menu principal e oculta o menu secundário
+    menuPrincipal.style.display = 'block';
+    menuSecundario.style.display = 'none';
   }
 });
